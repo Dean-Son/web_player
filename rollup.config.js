8a422741ed353db8c68f7afb44488330c8b779bc
dean(machson) <machson@kakaovx.com>
2025-09-04 08:37:12 +0900

    소스 복구

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.umd.js",
+      format: "umd",
+      name: "deanVideoPlayer",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+    postcss({
+      extract: false, // CSS를 JavaScript에 인라인으로 포함
+      inject: true, // 런타임에 CSS를 자동 주입
+      minimize: true,
+    }),
+  ],
+};
